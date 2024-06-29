3204644e13505cb7dec838804efde53a73d345ac
DearYuto <reasons@kakao.com>
2024-06-30 07:26:27 +0900

    refactor: useDragAndDrop 내부 if문 중복로직 제거

--- src/hooks/useDragAndDrop.ts
+++ src/hooks/useDragAndDrop.ts
@@ -1,6 +1,8 @@
-import { useState } from 'react';
-import { Column } from '@/types/column';
+import { useCallback, useState } from 'react';
 import { DropResult } from 'react-beautiful-dnd';
+
+import type { Column } from '@/types/column';
+
 import { getColumns } from '@/utils/getColumns';
 
 const NUM_COLUMNS = 4;
@@ -11,38 +13,33 @@ export const useDragAndDrop = () => {
     getColumns({ itemsPerColumn: ITEMS_PER_COLUMN, numColumns: NUM_COLUMNS })
   );
 
-  const onDragEnd = (result: DropResult) => {
-    const { source, destination } = result;
+  const onDragEnd = useCallback(
+    (result: DropResult) => {
+      const { source, destination } = result;
 
-    console.log(result);
+      if (!destination) {
+        return;
+      }
 
-    if (!destination) {
-      return;
-    }
+      const sourceColumnIndex = parseInt(source.droppableId.replace('column', ''), 10);
+      const destinationColumnIndex = parseInt(destination.droppableId.replace('column', ''), 10);
 
-    const sourceColumnIndex = parseInt(source.droppableId.replace('column', ''), 10);
-    const destinationColumnIndex = parseInt(destination.droppableId.replace('column', ''), 10);
+      const newSourceColumn = [...columns[sourceColumnIndex]];
+      const newDestinationColumn = [...columns[destinationColumnIndex]];
 
-    const newSourceColumn = [...columns[sourceColumnIndex]];
-    const newDestinationColumn = [...columns[destinationColumnIndex]];
-
-    if (sourceColumnIndex === destinationColumnIndex) {
       const [removed] = newSourceColumn.splice(source.index, 1);
       newSourceColumn.splice(destination.index, 0, removed);
-
-      const newColumns = Array.from(columns);
+      const newColumns = [...columns];
       newColumns[sourceColumnIndex] = newSourceColumn;
-      setColumns(newColumns);
-    } else {
-      const [removed] = newSourceColumn.splice(source.index, 1);
-      newDestinationColumn.splice(destination.index, 0, removed);
 
-      const newColumns = Array.from(columns);
-      newColumns[sourceColumnIndex] = newSourceColumn;
-      newColumns[destinationColumnIndex] = newDestinationColumn;
+      if (sourceColumnIndex !== destinationColumnIndex) {
+        newColumns[destinationColumnIndex] = newDestinationColumn;
+      }
+
       setColumns(newColumns);
-    }
-  };
+    },
+    [columns]
+  );
 
   return {
     columns,
