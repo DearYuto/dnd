ab6006666113ff55eb74a11efe2f71df648a43fc
DearYuto <reasons@kakao.com>
2024-06-30 08:10:32 +0900

    feat: 드래그 제약 조건 추가

--- src/hooks/useDragAndDrop.ts
+++ src/hooks/useDragAndDrop.ts
@@ -4,9 +4,34 @@ import { DropResult } from 'react-beautiful-dnd';
 import type { Column } from '@/types/column';
 
 import { getColumns } from '@/utils/getColumns';
+import { Item } from '@/types/item';
 
 const NUM_COLUMNS = 4;
 const ITEMS_PER_COLUMN = 10;
+
+const isMoveAllowed = (
+  sourceColumnIndex: number,
+  destinationColumnIndex: number,
+  sourceIndex: number,
+  destinationIndex: number,
+  columns: Column[]
+) => {
+  // 첫번째 컬럼 -> 세번째 컬럼 이동 불가
+  if (sourceColumnIndex === 0 && destinationColumnIndex === 2) {
+    return false;
+  }
+
+  const sourceItem = columns[sourceColumnIndex][sourceIndex];
+  const destinationItem = columns[destinationColumnIndex][destinationIndex];
+
+  // 짝수 아이템은 다른 짝수 아이템 이동 불가
+  const isEven = (item: Item) => parseInt(item.id) % 2 === 0;
+  if (isEven(sourceItem) && isEven(destinationItem)) {
+    return false;
+  }
+
+  return true;
+};
 
 export const useDragAndDrop = () => {
   const [columns, setColumns] = useState<Column[]>(
@@ -25,6 +50,19 @@ export const useDragAndDrop = () => {
       const newColumns = [...columns];
       const newSourceColumn = [...columns[sourceColumnIndex]];
       const newDestinationColumn = [...columns[destinationColumnIndex]];
+
+      if (
+        !isMoveAllowed(
+          sourceColumnIndex,
+          destinationColumnIndex,
+          source.index,
+          destination.index,
+          newColumns
+        )
+      ) {
+        // TODO 이동 불가 스타일 적용
+        return;
+      }
 
       const [removed] = newSourceColumn.splice(source.index, 1);
       newColumns[sourceColumnIndex] = newSourceColumn;
