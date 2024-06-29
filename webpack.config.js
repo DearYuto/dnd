4cc9c7f80ca19141342b400b709efade98a05c4c
DearYuto <reasons@kakao.com>
2024-06-30 00:45:47 +0900

    feat: webpack 세팅

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+import { fileURLToPath, URL } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+export default {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: `${__dirname}/dist`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: `${__dirname}/dist`,
+    },
+    compress: true,
+    port: 3000,
+    historyApiFallback: true,
+    open: true,
+  },
+};
