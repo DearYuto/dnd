2fd1373aecb98a3f752e9b2478b679bf4cc5e249
DearYuto <reasons@kakao.com>
2024-07-02 04:37:27 +0900

    feat: state와 setState 각각 컨텍스트 분리

--- src/context/SelectedItemsContext.tsx
+++ src/context/SelectedItemsContext.tsx
@@ -1,30 +1,47 @@
 import React, { createContext, useContext, useMemo, useState } from 'react';
 import { Id } from 'react-beautiful-dnd';
 
-type SelectedItemsContextProps = {
-  selectedItemIds: Id[];
-  setSelectedItemIds: React.Dispatch<React.SetStateAction<Id[]>>;
-};
+type SelectedItemsUpdateContextProps = React.Dispatch<React.SetStateAction<Id[]>>;
 
 type Props = {
   children: React.ReactNode;
 };
 
-const SelectedItemsContext = createContext<SelectedItemsContextProps | null>(null);
+const SelectedItemsContext = createContext<Id[] | null>(null);
+const SelectedItemsUpdateContext = createContext<SelectedItemsUpdateContextProps | null>(
+  () => null
+);
 
 export const SelectedItemsProvider = ({ children }: Props) => {
   const [selectedItemIds, setSelectedItemIds] = useState<Id[]>([]);
 
-  const value = useMemo(() => ({ selectedItemIds, setSelectedItemIds }), [selectedItemIds]);
+  const value = useMemo(() => selectedItemIds, [selectedItemIds]);
+  const update = useMemo(() => setSelectedItemIds, [setSelectedItemIds]);
 
-  return <SelectedItemsContext.Provider value={value}>{children}</SelectedItemsContext.Provider>;
+  return (
+    <SelectedItemsContext.Provider value={value}>
+      <SelectedItemsUpdateContext.Provider value={update}>
+        {children}
+      </SelectedItemsUpdateContext.Provider>
+    </SelectedItemsContext.Provider>
+  );
 };
 
-export const useSelectedItems = (): SelectedItemsContextProps => {
+export const useSelectedItemsValue = (): Id[] => {
   const context = useContext(SelectedItemsContext);
 
-  if (!context) {
-    throw new Error('context가 존재하지 않습니다.');
+  if (context === null) {
+    throw new Error('SelectedItemsValueContext가 존재하지 않습니다.');
+  }
+
+  return context;
+};
+
+export const useSelectedItemsUpdate = (): SelectedItemsUpdateContextProps => {
+  const context = useContext(SelectedItemsUpdateContext);
+
+  if (context === null) {
+    throw new Error('SelectedItemsUpdateContext가 존재하지 않습니다.');
   }
 
   return context;
