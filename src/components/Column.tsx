4a6bbe9a7b8c12f7deee99b4e1f5fe45926218e3
DearYuto <reasons@kakao.com>
2024-07-02 03:00:16 +0900

    feat: 아이템 클릭 시 선택된 아이템을 토글링할 상태, 클릭 이벤트 추가

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,8 +1,9 @@
-import { Droppable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+import { Droppable, Id } from 'react-beautiful-dnd';
 
-import { getListStyle } from '@/styles/getListStyle';
 import type { Column } from '@/types/column';
 import Item from './Item';
+import { useState } from 'react';
 
 type Props = {
   column: Column;
@@ -10,22 +11,44 @@ type Props = {
 };
 
 const Column = ({ droppableId, column }: Props) => {
+  const [selectedItemIds, setSelectedItemIds] = useState<Id[]>([]);
+
+  const onClick = (item: Item) => () => {
+    setSelectedItemIds((prevIds) => {
+      if (prevIds.includes(item.id)) {
+        return prevIds.filter((id) => id !== item.id);
+      }
+
+      return [...prevIds, item.id];
+    });
+  };
+
   return (
     <Droppable droppableId={`column${droppableId}`}>
       {(provided, snapshot) => (
-        <div
+        <ListContainer
+          $isDraggingOver={snapshot.isDraggingOver}
           {...provided.droppableProps}
           ref={provided.innerRef}
-          style={getListStyle(snapshot.isDraggingOver)}
         >
           {column.map((item, index) => (
-            <Item item={item} index={index} key={item.id} />
+            <Item onClick={onClick} item={item} index={index} key={item.id} />
           ))}
           {provided.placeholder}
-        </div>
+        </ListContainer>
       )}
     </Droppable>
   );
 };
 
 export default Column;
+
+const ListContainer = styled.div<{ $isDraggingOver: boolean }>`
+  margin: 10px;
+  background-color: ${({ $isDraggingOver }) => ($isDraggingOver ? '#cfd5ec' : '#e2e6f6')};
+  padding: 10px;
+  width: 300px;
+  min-height: 500px;
+  border-radius: 10px;
+  padding-top: 50px;
+`;
